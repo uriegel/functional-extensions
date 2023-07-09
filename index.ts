e207e38cb2c487810b3c02c36865a73e6c50b8fb
Uwe Riegel <uriegel@hotmail.de>
2023-07-09 12:46:25 +0200

    Now it exports everything

--- index.ts
+++ index.ts
@@ -62,3 +62,77 @@ declare global {
         contains(t: T): boolean
     }
 }
+
+// eslint-disable-next-line
+Array.prototype.sideEffectForEach = function<T> (sideEffect: (t: T)=>void): T[] {
+    this.forEach(sideEffect)
+    return this 
+}
+
+// eslint-disable-next-line
+Array.prototype.insert = function<T> (index: number, t: T): T[] {
+    return [...this.slice(0, index),
+        t,
+        ...this.slice(index)
+    ]
+}
+
+// eslint-disable-next-line
+Array.prototype.append = function<T> (t: T): T[] {
+    return [...this, t]
+}
+
+// eslint-disable-next-line
+Array.prototype.contains = function <T>(t: T): boolean {
+    return this.find(n => n === t)
+}
+
+// eslint-disable-next-line
+String.prototype.substringAfter = function (startChar: string): string {
+    const posStart = this?.indexOf(startChar) + 1 ?? -1
+    return posStart != -1 && posStart < this.length - 1
+    ? this.substring(posStart)
+    : ""
+}
+
+// eslint-disable-next-line
+String.prototype.substringUntil = function (endChar: string): string {
+    const posEnd = this?.indexOf(endChar) ?? 0
+    return posEnd > 0
+    ? this.substring(0, posEnd)
+    : this as string ?? ""
+}
+
+// eslint-disable-next-line
+String.prototype.stringBetween = function (startChar: string, endChar: string): string {
+    return this
+        ?.substringAfter(startChar)
+        ?.substringUntil(endChar)
+        ?? "";
+}
+
+// eslint-disable-next-line
+String.prototype.lastIndexOfAny = function (chars: string[]): number {
+    if (chars.length > 0) {
+        const res = this.lastIndexOf(chars[0])
+        return res != -1
+            ? res + 1
+            : this.lastIndexOfAny(chars.slice(1))
+    } else 
+        return -1
+}
+
+// eslint-disable-next-line
+String.prototype.sideEffect = function (sideEffect: (s: string)=>void): string {
+    sideEffect(this as string)
+    return this as string
+}
+
+// eslint-disable-next-line
+String.prototype.parseInt = function (): number|null {
+    var result = Number.parseInt(this as string)
+    return Number.isNaN(result)
+        ? null
+        : result
+}
+
