b97b5700e191d16cd477b9bee21edc9caad3dd9e
Uwe Riegel <uriegel@hotmail.de>
2025-10-31 14:10:05 +0100

    retryOnError

--- extensions/error.ts
+++ extensions/error.ts
@@ -2,23 +2,18 @@
  * Calling a function. If the function throws an exception, onError is called to react to the exception e. 
  * If it can be corrected, onError returns, otherwise onError must throw e. If it does not fail, action is called again
  * @param action The action which will be called, and again, if onError does not fail
- * @param onError If action throws an error, this function is called. You can throw the original error, or you can solve the problem, then it retries. 
- * If you call 'cancel()' and throw the original error, t will not retry any more
+ * @param onError If action throws an error, this function is called. If it corrects the problem return, otherwise throw an exception
  * @param retryCount Number of trials, 1 is default
  * @returns a result of type T
  */
-export const retryOnError = <T>(action: () => T, onError: (e: unknown, cancel: ()=>void) => boolean, retryCount = 1) => {
+export function retryOnError<T>(action: () => Promise<T>, onError: (e: unknown)=>void, retryCount = 1) {
     for (let n = 0; n <= retryCount; n++) {
         try {
             return action()
         } catch (e) {
-            let cancel = false
-            try {
-                onError(e, () => { cancel = true })
-            } catch (e) {
-                if (cancel || n == retryCount - 1)
-                    throw e
-            }
+            if (n == retryCount)
+                throw e
+            onError(e)
         }
     }
     throw "too many iterations"
@@ -28,23 +23,18 @@ export const retryOnError = <T>(action: () => T, onError: (e: unknown, cancel: (
  * Calling a function. If the function throws an exception, onError is called to react to the exception e. 
  * If it can be corrected, onError returns, otherwise onError must throw e. If it does not fail, action is called again
  * @param action The action which will be called, and again, if onError does not fail
- * @param onError If action throws an error, this function is called. You can throw the original error, or you can solve the problem, then it retries. 
- * If you call 'cancel()' and throw the original error, t will not retry any more
+ * @param onError If action throws an error, this function is called. If it corrects the problem return, otherwise throw an exception
  * @param retryCount Number of trials, 1 is default
  * @returns a result of type T
  */
-export const retryOnErrorAsync = async <T>(action: () => Promise<T>, onError: (e: unknown, cancel: ()=>void)=>Promise<void>, retryCount = 1) => {
+export async function retryOnErrorAsync<T>(action: () => Promise<T>, onError: (e: unknown)=>Promise<void>, retryCount = 1) {
     for (let n = 0; n <= retryCount; n++) {
         try {
             return await action()
         } catch (e) {
-            let cancel = false
-            try {
-                await onError(e, () => cancel = true)
-            } catch (e) {
-                if (cancel || n == retryCount - 1)
-                    throw e
-            }
+            if (n == retryCount)
+                throw e
+            await onError(e)
         }
     }
     throw "too many iterations"
